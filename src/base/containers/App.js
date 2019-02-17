abaa451e1af6f8c59d8c3b8c4f6899ea0129efbb
Jacek Lankauf <jacek.lankauf@gmail.com>
2019-02-17 19:05:06 +0100

    project configuration

---
+++ src/base/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainView from '../layout/MainView';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/" component={MainView} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
+
---
+++ src/base/layout/MainView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainView extends React.Component {
+  render() {
+    return (
+      <div>
+        main view
+      </div>
+    )
+  }
+}
