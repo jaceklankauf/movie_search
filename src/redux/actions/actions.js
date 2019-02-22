ffaa6fd8179b2e4a689ed2205785dff4b8f8d767
Jacek Lankauf <jacek.lankauf@gmail.com>
2019-02-22 18:05:02 +0100

    Search component

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -3,10 +3,14 @@ import {
   API_KEY,
   URL_MOVIE_DETAILS
 } from '../utils';
+import { URL_SEARCH } from '../../modules/const';
 
 export const FETCH_MOVIES = 'FETCH_MOVIES';
 export const FETCH_MOVIES_SUCCESS = 'FETCH_MOVIES_SUCCESS';
 export const FETCH_MOVIES_ERROR = 'FETCH_MOVIES_ERROR';
+export const SEARCH_MOVIE = 'SEARCH_MOVIE';
+export const SEARCH_MOVIE_SUCCESS = 'SEARCH_MOVIE_SUCCESS';
+export const SEARCH_MOVIE_ERROR = 'SEARCH_MOVIE_ERROR';
 
 function fetchMovies() {
   return {
@@ -28,6 +32,26 @@ function fetchMoviesError(error) {
   };
 }
 
+function searchMovie() {
+  return {
+    type: SEARCH_MOVIE
+  };
+}
+
+function searchMovieSuccess(data) {
+  return {
+    type: SEARCH_MOVIE_SUCCESS,
+    data
+  };
+}
+
+function searchMovieError(error) {
+  return {
+    type: SEARCH_MOVIE_ERROR,
+    error
+  };
+}
+
 export function fetchMovieList() {
   const url = URL_LIST + API_KEY;
   return function (dispatch) {
@@ -40,9 +64,22 @@ export function fetchMovieList() {
   };
 }
 
+export function searchMoviesList(title){
+  let url = URL_SEARCH + API_KEY + '&query=' + title;
+  return function(dispatch){
+    dispatch(searchMovie())
+    return fetch(url)
+      .then(response => response.json())
+      .then(json => json.results)
+      .then(data => dispatch(searchMovieSuccess(data,title)))
+      .catch(error => dispatch(searchMovieError(error)))
+  }
+}
+
 export const FETCH_MOVIE = 'FETCH_MOVIE';
 export const FETCH_MOVIE_SUCCESS = 'FETCH_MOVIE_SUCCESS';
 export const FETCH_MOVIE_ERROR = 'FETCH_MOVIE_ERROR';
+
 
 function fetchMovie() {
   return {
